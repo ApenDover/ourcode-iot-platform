26605718d9c7ea418b9bfc1b41cbd112c5386099
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-08-12 22:45:46 +0300

    clean up

--- event-collector/src/main/java/ts/andrey/eventcollector/cassandra/dataService/DeviceEventDataService.java
+++ event-collector/src/main/java/ts/andrey/eventcollector/cassandra/dataService/DeviceEventDataService.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ts.andrey.eventcollector.cassandra.entity.DeviceEventEntity;
-import ts.andrey.eventcollector.cassandra.entity.DeviceEventKey;
 import ts.andrey.eventcollector.cassandra.repository.DeviceEventRepository;
-import ts.andrey.eventcollector.exception.ExceptionMessage;
-import ts.andrey.eventcollector.exception.IotException;
 
 import java.util.List;
 
@@ -16,35 +13,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DeviceEventDataService {
 
-    private static final String EVENT_ID = "eventId";
-
     private final DeviceEventRepository repository;
-
-    public DeviceEventEntity save(DeviceEventEntity event) {
-        final var saved = repository.save(event);
-        log.debug("Сохранил событие в кассандра: {}", saved);
-        return saved;
-    }
 
     public List<DeviceEventEntity> saveAll(List<DeviceEventEntity> events) {
         final var saved = repository.saveAll(events);
         log.debug("Сохранил события в кассандра: {}", events.size());
         return saved;
-    }
-
-    public DeviceEventEntity getByEventId(DeviceEventKey key) {
-        final var deviceEvent = repository.findByKeyComponents(
-                key.getDeviceId(),
-                key.getTimestamp(),
-                key.getEventId()
-        );
-        if (deviceEvent.isEmpty()) {
-            throw new IotException(
-                    String.format(ExceptionMessage.CASSANDRA_DEVICE_EVENT_NOT_FOUND.getValue(),
-                            EVENT_ID, key.getEventId())
-            );
-        }
-        return deviceEvent.get();
     }
 
     public boolean isExistDeviceId(String deviceId) {
--- event-collector/src/main/java/ts/andrey/eventcollector/exception/IotException.java
+++
@@ -1,9 +0,0 @@
-package ts.andrey.eventcollector.exception;
-
-public class IotException extends RuntimeException {
-
-    public IotException(String message) {
-        super(message);
-    }
-
-}
