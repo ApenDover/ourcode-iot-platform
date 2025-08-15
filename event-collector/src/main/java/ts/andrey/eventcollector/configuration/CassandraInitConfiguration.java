7f60ae0e566ba7395f5ad089f2a14ede14396176
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-08-15 10:26:33 +0300

    sql инекция, для отважных

--- event-collector/src/main/java/ts/andrey/eventcollector/configuration/CassandraInitConfiguration.java
+++ event-collector/src/main/java/ts/andrey/eventcollector/configuration/CassandraInitConfiguration.java
@@ -1,64 +1,43 @@
 package ts.andrey.eventcollector.configuration;
 
 import com.datastax.oss.driver.api.core.CqlSession;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
-import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SessionBuilderConfigurer;
 
-import java.net.InetSocketAddress;
-import java.util.Collections;
-
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @Profile("!test")
-public class CassandraInitConfiguration {
+public class CassandraInitConfiguration extends AbstractCassandraConfiguration {
 
     private static final String CQL_INIT = "CREATE KEYSPACE IF NOT EXISTS %s "
-            + "WITH replication = {'class':'SimpleStrategy','replication_factor':'1'};";
-
-    @Value("${spring.cassandra.contact-points}")
-    private String contactPoints;
+            + "WITH replication = {%s};";
 
     @Value("${spring.cassandra.keyspace-name}")
     private String keyspace;
 
-    @Value("${spring.cassandra.port}")
-    private int port;
+    @Value("${spring.cassandra.replication-strategy}")
+    private String replicationStrategy;
 
-    @Value("${spring.cassandra.local-datacenter}")
-    private String localDatacenter;
-
-    @Bean
-    public CqlSessionFactoryBean session() {
-        CqlSessionFactoryBean session = new CqlSessionFactoryBean();
-        session.setContactPoints(contactPoints);
-        session.setPort(port);
-        session.setLocalDatacenter(localDatacenter);
-        session.setKeyspaceCreations(Collections.emptyList());
-        return session;
+    @Override
+    protected String getKeyspaceName() {
+        return keyspace;
     }
 
-    @Bean
-    public CassandraTemplate cassandraTemplate(CqlSession session) {
-        session.execute(String.format(CQL_INIT, keyspace));
-        session.execute("USE " + keyspace);
-        return new CassandraTemplate(session);
-    }
-
-    @PostConstruct
-    public void init() {
-        try (CqlSession session = CqlSession.builder()
-                .addContactPoint(
-                        new InetSocketAddress(contactPoints, port))
-                .withLocalDatacenter(localDatacenter)
-                .build()) {
-            session.execute(String.format(CQL_INIT, keyspace));
-        }
+    @Override
+    public SessionBuilderConfigurer getSessionBuilderConfigurer() {
+        log.info("Initializing Cassandra keyspace {}, command={}", keyspace, String.format(CQL_INIT, keyspace, replicationStrategy));
+        return cqlSessionBuilder -> {
+            try (CqlSession tempSession = cqlSessionBuilder.build()) {
+                tempSession.execute(String.format(CQL_INIT, keyspace, replicationStrategy));
+            }
+            return cqlSessionBuilder.withKeyspace(keyspace);
+        };
     }
 
 }
