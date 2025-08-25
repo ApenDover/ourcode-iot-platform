58fad08fe1f424ffa059b1982c46d294d72472c1
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-08-25 09:51:19 +0300

    liq remove

--- device-collector/src/main/java/ts/andrey/devicecollector/DeviceCollectorApplication.java
+++ device-collector/src/main/java/ts/andrey/devicecollector/DeviceCollectorApplication.java
@@ -3,14 +3,12 @@ package ts.andrey.devicecollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
-import ts.andrey.devicecollector.utils.LiquibaseProcessor;
 
 @SpringBootApplication
 @EnableRetry
 public class DeviceCollectorApplication {
 
     public static void main(String[] args) {
-        LiquibaseProcessor.run(args);
         SpringApplication.run(DeviceCollectorApplication.class, args);
     }
 
