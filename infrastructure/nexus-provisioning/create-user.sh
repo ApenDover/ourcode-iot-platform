45bc016398e7ea112c76bb180e6a1383f1aa9329
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-08-27 13:04:07 +0300

    nexus

---
+++ infrastructure/nexus-provisioning/create-user.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd /opt/sonatype/nexus
+
+./bin/nexus run &
+
+echo "Waiting for Nexus to be ready..."
+until curl -sf http://localhost:8081 > /dev/null; do
+    echo "Nexus not ready yet, sleeping 5s..."
+    sleep 5
+done
+echo "Nexus is ready ✅"
+
+PASSWORD="${NEXUS_USER_PASSWORD:-user}"
+USER="${NEXUS_USER:-user}"
+
+echo "Создаем скрипт create-user"
+curl -s -u admin:admin123 -X POST "http://localhost:8081/service/rest/v1/script" \
+    -H "Content-Type: application/json" \
+    -d "{
+        \"name\": \"create-user\",
+        \"type\": \"groovy\",
+        \"content\": \"security.addUser('$USER', 'user', 'user', 'user@mail', true, '$PASSWORD', ['nx-all']);\"
+    }"
+
+
+#echo "Запускаем Groovy скрипт create-admin.groovy"
+#curl -s -u admin:${NEXUS_ADMIN_PASSWORD} \
+#  -H "Content-Type: text/plain" \
+#  -X POST "http://localhost:8081/service/rest/v1/script/create-user/run"
+
+curl --location --request POST 'http://localhost:8081/service/rest/v1/script/create-user/run' \
+--header 'Content-Type: text/plain' \
+--header 'Authorization: Basic YWRtaW46YWRtaW4xMjM=' \
+--data ''
+
+wait
