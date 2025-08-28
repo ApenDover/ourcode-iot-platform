a8d97eb1fe4cb86e64edceeb536656ffb7bd5bcc
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-08-28 23:45:18 +0300

    in p

--- device-service/src/main/java/ts/andrey/deviceservice/configuration/security/SecurityConfig.java
+++ device-service/src/main/java/ts/andrey/deviceservice/configuration/security/SecurityConfig.java
@@ -14,7 +14,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/api/v1/devices/**").authenticated()
+//                                .requestMatchers("/api/v1/devices/**").authenticated()
+                        .requestMatchers("/actuator/**").hasAuthority("ROLE_deviceapp.admin")
+                        .requestMatchers("/api/v1/devices/**").hasAnyAuthority("ROLE_deviceapp.user", "ROLE_deviceapp.admin")
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
