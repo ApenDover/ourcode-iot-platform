84dd71e0750e2645e444daa3a6b4e0730349eff4
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-08-28 13:14:25 +0300

    keycloak start

---
+++ device-service/src/main/java/ts/andrey/deviceservice/configuration/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package ts.andrey.deviceservice.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String ADMIN_ROLE = "ADMIN";
+    private static final String USER_ROLE = "USER";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/devices/**").hasRole(ADMIN_ROLE)
+                        .requestMatchers(HttpMethod.POST, "/api/v1/devices").hasAnyRole(USER_ROLE, ADMIN_ROLE)
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/devices/**").hasAnyRole(USER_ROLE, ADMIN_ROLE)
+                        .requestMatchers(HttpMethod.GET, "/api/v1/devices/**").hasAnyRole(USER_ROLE, ADMIN_ROLE)
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+
+}
