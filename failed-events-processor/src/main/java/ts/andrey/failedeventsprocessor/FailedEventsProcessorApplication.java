8f6085dcf6b5bc1bb0f578b18664811359434225
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-09-02 12:55:10 +0300

    fix

--- failed-events-processor/src/main/java/ts/andrey/failedeventsprocessor/FailedEventsProcessorApplication.java
+++ failed-events-processor/src/main/java/ts/andrey/failedeventsprocessor/FailedEventsProcessorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FailedEventsProcessorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FailedEventsProcessorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FailedEventsProcessorApplication.class, args);
+    }
 
 }
