3d1fd96dc087913bff768cb0987b0b884679632a
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-09-02 11:03:28 +0300

    новый модуль

---
+++ failed-events-processor/src/main/java/ts/andrey/failedeventsprocessor/FailedEventsProcessorApplication.java
@@ -0,0 +1,13 @@
+package ts.andrey.failedeventsprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailedEventsProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FailedEventsProcessorApplication.class, args);
+	}
+
+}
