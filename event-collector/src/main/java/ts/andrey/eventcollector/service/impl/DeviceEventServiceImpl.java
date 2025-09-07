db6c334cc327cd5c1f7fc8eff2d203cc37e0507b
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-09-07 12:51:42 +0300

    in p

--- event-collector/src/main/java/ts/andrey/eventcollector/service/impl/DeviceEventServiceImpl.java
+++ event-collector/src/main/java/ts/andrey/eventcollector/service/impl/DeviceEventServiceImpl.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ts.andrey.eventcollector.data.dao.DeviceEventDataService;
-import ts.andrey.eventcollector.mapper.DeviceEventMapper;
 import ts.andrey.eventcollector.service.DeviceEventService;
 import ts.andrey.eventcollector.service.component.SimpleCache;
 
