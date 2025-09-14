a7927edfe0859cbbc634327e88892e43234053ff
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-09-14 21:47:39 +0300

    metrics

--- router-mananger-service/cmd/app/main.go
+++ router-mananger-service/cmd/app/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"golang.org/x/net/context"
 	"log/slog"
+	"net/http"
 	"router-mananger-service/config"
 	"router-mananger-service/internal/conf"
 	"router-mananger-service/internal/util"
@@ -15,8 +17,17 @@ func main() {
 	dbPath := databasePath()
 	pool, err := pgxpool.New(context.Background(), dbPath)
 	if err != nil {
-		log.Error("Не смог подключиться к БД", slog.String("error", err.Error()))
+		log.Error("ошибка подключения к БД", slog.String("error", err.Error()))
 	}
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		err := http.ListenAndServe(":9091", nil)
+		if err != nil {
+			log.Error("не смог запустить экспорт метрик", slog.String("error", err.Error()))
+			return
+		}
+	}()
+
 	conf.InitGrpc(pool, dbPath)
 }
 
